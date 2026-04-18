2395dd926badf0f1a47a764955af86a8573ad769
LuisHerenu11 <ElCondor_1106@hotmail.com>
2026-04-17 21:04:46 -0300

    ejercicios clase tres

--- clase3/index.js
+++ clase3/index.js
@@ -1,1 +1,7 @@
-console.log("¡Hola, mi proyecto Node.js está funcionando!");
+let preciosProductos = [2,4,8,16,32,64,128,256,512,1024]
+
+let preciosProductosConIva = preciosProductos.map((precio) => precio *= 1.21)
+
+preciosProductosConIva.forEach((elemento) =>
+    console.log(`El precio es: ${elemento}.- IVA incluido.`)
+)
